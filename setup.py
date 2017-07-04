f87d8619b87e69fb67f278f46524829cf18320b1
vition <369709991@qq.com>
2017-07-04 14:59:39 +0800

    setup更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # @Author: vition
 # @Date:   2017-07-04 14:19:51
 # @Last Modified by:   vition
-# @Last Modified time: 2017-07-04 14:28:24
+# @Last Modified time: 2017-07-04 14:58:16
 from distutils.core import setup
 
 setup(
@@ -10,7 +10,7 @@ setup(
     version='1.2.0',
     author='vition kuo',
     author_email='369709991@qq.com',
-    packages=['WorkingDay', 'ChinaHoliday'],
+    scripts=['WorkingDay.py','ChinaHoliday.py'],
     url='https://github.com/vition/workingDay',
     description='获取节假日的库，适用于中国',
     long_description=open('README.md').read(),
